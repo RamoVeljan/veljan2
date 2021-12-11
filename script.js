7c6b2131fe7226f827a9a65bc7e4d7a88c329b92
veljan.r <95553450+RamoVeljan@users.noreply.github.com>
2021-12-11 21:13:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+console.log(`Program se ucitava.....`);
 
-var ime = `Ramo`;
+var ime = `Ramo `;
 var prezime = `Veljan`;
 var iip = ime.concat(prezime);
 
 for (var i = 0; i < iip.length; i++) {
     if(i%2===0){
-        document.write("<span style='color:red;'>" +iip[i]+ "</span>");
+        document.write("<span style='color:black;'>" +iip[i]+ "</span>");
     }
     else{
-        document.write("<span style='color:black';>" +iip[i]+ "</span>")
+        document.write("<span style='color:red';>" +iip[i]+ "</span>")
     }
 }
